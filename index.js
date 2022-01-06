cf79939c271302e0289cd779cbadb4743691049b
Ramkumar492 <49348196+Ramkumar492@users.noreply.github.com>
2022-01-06 13:05:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+document.addEventListener("keydown",function(event)
+{
+  if(event.key=="Enter")
+  {
+    roll();
+  }
+});
+
+var rollbtn= document.querySelector("button");
+
+rollbtn.addEventListener("click", roll);
+
+function roll()
+{
+  var random1= Math.floor((Math.random()*6))+1;
+  var random2= Math.floor((Math.random()*6))+1;
+
+  var image1= "images/dice"+random1+".png";
+  var image2= "images/dice"+random2+".png";
+
+  document.querySelectorAll("img")[0].setAttribute("src",image1); //for image1
+  document.querySelectorAll("img")[1].setAttribute("src",image2); //for image2
+
+  if(random1>random2)
+  {
+    document.querySelector("h1").textContent="🚩Player1 Wins";
+  }
+  else if (random1<random2) {
+
+    document.querySelector("h1").textContent="Player2 Wins🚩";
+
+  }
+  else
+  {
+      document.querySelector("h1").textContent="Draw 🏳";
+  }
+
+}
